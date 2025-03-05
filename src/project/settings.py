bfa5994af8eae16bce7829e8393e10301859737b
lucaskaiiser <lucaskaiiser@live.com>
2025-03-04 23:26:21 -0400

    adjust on the db connection

--- src/project/settings.py
+++ src/project/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'postgres',
+        "USER": 'admin',
+        "PASSWORD": "admin",
+        "HOST": "db_blog",
+        "PORT": "5432"
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
