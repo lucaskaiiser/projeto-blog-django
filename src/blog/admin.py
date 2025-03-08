d385091b4766c6ce44da8277ea90bf4d3ebcc86d
lucaskaiiser <lucaskaiiser@live.com>
2025-03-07 21:08:38 -0400

    added model and model admin for Post

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Tag, Category, Page
+from blog.models import Tag, Category, Page, Post
 # Register your models here.
 
 @admin.register(Tag)
@@ -34,3 +34,26 @@ class PageAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         "slug": ('title',),
     }
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'is_published','created_by'
+    list_display_links = 'title',
+    search_fields = 'id', 'title', 'is_published','slug', 'excerpt', 'content',
+    list_filter = 'category', 'is_published',
+    list_editable = 'is_published',
+    list_per_page = 50
+    ordering = '-id',
+    prepopulated_fields = {
+        "slug": ('title',),
+    }
+    autocomplete_fields = 'category', 'tags',
+    readonly_fields = 'created_at', 'created_by', 'updated_at', 'updated_by',
+
+    def save_model(self, request, obj: Post, form, change):
+        if change:
+            obj.updated_by = request.user
+        else:
+            obj.created_by = request.user
+            obj.updated_by = request.user
+        return super().save_model(request, obj, form, change)
--- src/blog/models.py
+++ src/blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils.text import slugify
+from django.contrib.auth.models import User
+
 # Create your models here.
+
 class Tag(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(
@@ -15,6 +18,9 @@ class Tag(models.Model):
             self.slug = slugify(self.name)
         return super().save(*args,**kwargs)
     
+    def __str__(self):
+        return self.name
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(
@@ -28,6 +34,9 @@ class Category(models.Model):
         if not self.slug:
             self.slug = slugify(self.name)
         return super().save(*args,**kwargs)
+    
+    def __str__(self):
+        return self.name
     
 class Page(models.Model):
     title = models.CharField(max_length=50)
@@ -46,3 +55,33 @@ class Page(models.Model):
             self.slug = slugify(self.title)
         return super().save(*args,**kwargs)
     
+    def __str__(self):
+        return self.title
+    
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(
+        unique=True,
+        default=None,
+        null=True,
+        blank=True
+    )
+    is_published = models.BooleanField(default=True)
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts/', blank=True, default='')
+    cover_in_post = models.BooleanField(default=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, blank=True, null=True
+    )
+    tags = models.ManyToManyField(Tag, blank=True, default='')
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True,
+        related_name='post_created_by'
+    )
+    updated_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True,
+        related_name='post_updated_by'
+    )
